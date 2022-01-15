6cc817bdc2d0e1183abfdbfb704d1481c0c9bd2b
Tatyanaver <tpetrunya@ukr.net>
2022-01-15 21:59:52 +0200

    cw1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import {useEffect, useState} from "react";
 import './App.css'
 
-import Users from "./components/Users";
-import Posts from "./components/Posts";
-import Comments from "./components/Comments";
+import Flights from "./components/Flights";
 
 function App() {
-
    return (
-      <div className="wrap">
-        <Users/>
-        <Posts/>
-        <Comments/>
+      <div>
+          <Flights/>
+
       </div>
   );
 }
